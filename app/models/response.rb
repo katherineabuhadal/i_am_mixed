7b3ddd88938e1e3bb90034ddc5ad22afe2bc6372
katherineabuhadal <katherine8@verizon.net>
2014-08-15 13:23:18 -0400

    Edits

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,8 +1,8 @@
 class Response < ActiveRecord::Base
   belongs_to :user
   belongs_to :story
-  belongs_to :parent, class_name: 'Response'
-  has_many :children, class_name: 'Response', foreign_key: 'parent_id', dependent: :destroy
+  belongs_to :parent, class_name: "Response"
+  has_many :children, class_name: "Response", foreign_key: "parent_id", dependent: :destroy
 
   validates :body, presence: true
   validates :user, presence: true
